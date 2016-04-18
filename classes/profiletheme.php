dd6f1afa835344b435dc7ebfdc85309ff349d4cf
Alexander Bias <alexander.bias@uni-ulm.de>
2016-04-18 21:43:11 +0200

    Change form intro paragraph to a standard intro box

--- classes/profiletheme.php
+++ classes/profiletheme.php
@@ -44,7 +44,7 @@ class profiletheme extends profilefields {
      */
     public function output_form() {
         $out = '';
-        $out .= html_writer::tag('p', get_string('intro', 'local_profiletheme'));
+        $out .= html_writer::tag('div', get_string('intro', 'local_profiletheme'), array('id' => 'intro', 'class' => 'box generalbox'));
         $out .= parent::output_form();
         return $out;
     }
